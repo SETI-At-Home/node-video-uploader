6dc6b68cec75633f00bb0ce6a257c1fb41e2bb49
Vladimir Bozhinovski <bozhinovski.vladimir@gmail.com>
2017-10-16 17:28:42 +0200

    Adding basic node and mongo functionalities and route configurations

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/video-ideas-dev', {
+    useMongoClient: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
+
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Index Route
+app.get('/', (req, res) => {
+    const title = 'Welcome';
+    res.render('index', {
+        title: title
+    });
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+// About Route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
